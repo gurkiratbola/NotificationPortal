e7b0501049d7b5128f175f22cd71b91af4d64a8e
tylerblair <tblair18@my.bcit.ca>
2017-04-28 12:45:32 -0700

    Fixing structure of Server Stuff

---
+++ NotificationPortal/NotificationPortal/Repositories/IInterfaceRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationPortal.Repositories
+{
+   
+        public interface IInterfaceRepo<T> where T : class
+        {
+
+            IQueryable<T> GetAll();
+            //IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
+            T FindBy(int Id);
+            void Add(T entity);
+            void Delete(T entity);
+            void Edit(T entity);
+            void Save();
+            void Dispose();
+       
+        }
+}
