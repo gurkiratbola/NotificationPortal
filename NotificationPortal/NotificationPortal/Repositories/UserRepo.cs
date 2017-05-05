60e8b329d1a90881e953d7013be47ac5e6eadbfb
gurkiratbola <guri_bola@hotmail.com>
2017-05-05 09:13:07 -0700

    Update UserRepo.cs

--- NotificationPortal/NotificationPortal/Repositories/UserRepo.cs
+++ NotificationPortal/NotificationPortal/Repositories/UserRepo.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using NotificationPortal.Models;
