f530b1aae66b2b19868f669efa6c79ad5d822061
dakotaJang <djang36@my.bcit.ca>
2017-05-14 18:13:16 -0700

    multi select for notifcation create/edit

--- NotificationPortal/NotificationPortal/Scripts/notification-helper.js
+++ NotificationPortal/NotificationPortal/Scripts/notification-helper.js
@@ -1,1 +1,34 @@
 ﻿
+var filterAppsBasedOnServer = function () {
+    var serverRefIDs = $('#ServerList').val();
+    $(".ApplicationListItem").hide();
+    $.each(serverRefIDs, function (index, value) {
+        $("." + value).show();
+    });
+}
+var setupFilterDropDown = function () {
+    $('#ServerList').multiselect({
+        enableCaseInsensitiveFiltering: true,
+        includeSelectAllOption: true,
+        buttonText: function (options, select) {
+            return 'Server (' + options.length + ')';
+        }
+    });
+
+    $('#ApplicationReferenceIDs').multiselect({
+        enableCaseInsensitiveFiltering: true,
+        includeSelectAllOption: true,
+        buttonText: function (options, select) {
+            return 'Application (' + options.length + ')';
+        }
+    });
+
+    $("#ServerList").change(function () {
+        filterAppsBasedOnServer();
+    });
+};
+
+$(document).ready(function () {
+    setupFilterDropDown();
+    filterAppsBasedOnServer();
+});
