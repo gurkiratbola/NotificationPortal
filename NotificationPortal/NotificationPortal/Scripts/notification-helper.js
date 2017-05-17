6e596b1798c75f0b69c82c02e8efb5405241eabd
dakotaJang <djang36@my.bcit.ca>
2017-05-17 10:26:49 -0700

    implemented ajax for get application list based selected server while creating new notifications

--- NotificationPortal/NotificationPortal/Scripts/notification-helper.js
+++ NotificationPortal/NotificationPortal/Scripts/notification-helper.js
@@ -1,23 +1,70 @@
-﻿
-var filterAppsBasedOnServer = function () {
-    var serverRefIDs = $('#ServerList').val();
-    $(".ApplicationListItem").hide();
-    $.each(serverRefIDs, function (index, value) {
-        $("." + value).show();
+﻿// used for CreateThread,Create,Edit notification views
+
+// global variable to check if the page is initialized
+var initializeNotificationCreateView = false;
+// get all app based on selected servers
+var getAppsBasedOnServer = function () {
+    var serverReferenceIDs = $('#ServerList').val();
+    $('#preloader').show();
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        // domain is defined in ~/Scripts/script.js
+        url: domain + "api/Application",
+        data: JSON.stringify(serverReferenceIDs),
+        contentType: 'application/json',
+        success: function (data) {
+            if (initializeNotificationCreateView) {
+                refreshApplicationSelectOption(data);
+            } else {
+                loadApplicationSelectOption(data);
+            }
+            setupApplicationFilterDropDown()
+            hidePreloader();
+            initializeNotificationCreateView = true;
+        },
+        error: function (error) {
+            jsonValue = jQuery.parseJSON(error.responseText);
+            alert("error" + error.responseText);
+            hidePreloader();
+        }
     });
 }
-var setupFilterDropDown = function () {
+
+// setup the multi select plugin for the Server dropdown
+// also disable the server dropdown menu items when value is changed (i.e. send new request)
+var setupServerFilterDropDown = function () {
     $('#ServerList').multiselect({
         enableCaseInsensitiveFiltering: true,
         includeSelectAllOption: true,
         buttonText: function (options, select) {
             return 'Server (' + options.length + ')';
-        },
-        onChange: function (option, checked, select) {
-            filterAppsBasedOnServer();
         }
     });
+    $('#ServerList').change(function (option, checked, select) {
+        $('#ServerList option').each(function () {
+            var input = $('input[value="' + $(this).val() + '"]');
+            input.prop('disabled', true);
+            input.parent('li').addClass('disabled');
+        });
+        getAppsBasedOnServer();
+    });
+};
 
+// refresh the application dropdown
+var refreshApplicationSelectOption = function (data) {
+    $('#ApplicationList').replaceWith(`<div id="ApplicationList">
+                <select style="display: none;" class="form-control valid" id="ApplicationReferenceIDs" name="ApplicationReferenceIDs" multiple="multiple" aria-invalid="false"></select>
+                <span class="field-validation-valid text-danger" data-valmsg-for="ApplicationReferenceIDs" data-valmsg-replace="true"></span>
+            </div>`);
+    $.each(data, function (index, app) {
+        $('<option value="' + app.ReferenceID + '" class="ApplicationListItem">' + app.ApplicationName + '</option>').appendTo($('#ApplicationReferenceIDs'))
+    })
+}
+
+// setup the multi select plugin for the Application dropdown
+// also re-enable the server dropdown menu items
+var setupApplicationFilterDropDown = function () {
     $('#ApplicationReferenceIDs').multiselect({
         enableCaseInsensitiveFiltering: true,
         includeSelectAllOption: true,
@@ -25,9 +72,29 @@ var setupFilterDropDown = function () {
             return 'Application (' + options.length + ')';
         }
     });
-};
+    $('#ServerList option').each(function () {
+        var input = $('input[value="' + $(this).val() + '"]');
+        input.prop('disabled', false);
+        input.parent('li').addClass('disabled');
+    });
+}
+
+var loadApplicationSelectOption = function (data) {
+    $('#ApplicationList').replaceWith(`<div id="ApplicationList">
+                <select style="display: none;" class="form-control valid" id="ApplicationReferenceIDs" name="ApplicationReferenceIDs" multiple="multiple" aria-invalid="false"></select>
+                <span class="field-validation-valid text-danger" data-valmsg-for="ApplicationReferenceIDs" data-valmsg-replace="true"></span>
+            </div>`);
+    $.each(data, function (index, app) {
+        $('<option selected value="' + app.ReferenceID + '" class="ApplicationListItem">' + app.ApplicationName + '</option>').appendTo($('#ApplicationReferenceIDs'))
+    })
+}
+
+// hide preloader
+var hidePreloader = function () {
+    $('#preloader').hide();
+}
 
 $(document).ready(function () {
-    setupFilterDropDown();
-    filterAppsBasedOnServer();
+    setupServerFilterDropDown();
+    getAppsBasedOnServer();
 });
