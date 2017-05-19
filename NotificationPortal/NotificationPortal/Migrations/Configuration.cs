ee332a854b93d7294e56ad32304d1a710845ff0a
dakotaJang <djang36@my.bcit.ca>
2017-05-18 20:35:33 -0700

    Seed only reference tables and one admin. Removed all garbage data in the seed.

--- NotificationPortal/NotificationPortal/Migrations/Configuration.cs
+++ NotificationPortal/NotificationPortal/Migrations/Configuration.cs
@@ -10,15 +10,10 @@ namespace NotificationPortal.Migrations
 
     internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
-        private string sampleAdminEmail = "admin@portal.com";
-        private string sampleClientEmail = "client@portal.com";
-        private string sameplUserEmail = "user@portal.com";
-        private string sampleApplicationName1 = "Bank App";
-        private string sampleApplicationName2 = "Traffic App";
-        private string sampleApplicationName3 = "Domain App";
-        private string sampleServerName1 = "DNS Server";
-        private string sampleServerName2 = "App Server";
-        private string sampleServerName3 = "Web-App Server";
+        private string adminEmail = "admin@portal.com";
+        private string adminPassword = "password";
+        private string adminFirstName = "Elliot";
+        private string adminLastName = "Alderson";
 
         public Configuration()
         {
@@ -38,15 +33,11 @@ namespace NotificationPortal.Migrations
             SeedGroups(context);
             SeedStatusType(context);
             SeedServerType(context);
-
             SeedRoles(context);
             SeedRoleDetail(context);
             SeedStatus(context);
-            SeedClient(context);
-            SeedUsers(context);
-            SeedServer(context);
-            SeedApplication(context);
-            SeedNotification(context);
+
+            SeedAdmin(context);
         }
 
         private void SeedSendMethod(ApplicationDbContext context)
@@ -338,373 +329,43 @@ namespace NotificationPortal.Migrations
             context.SaveChanges();
         }
 
-        private void SeedClient(ApplicationDbContext context)
+        private void SeedAdmin(ApplicationDbContext context)
         {
-            var bcitStatus = context.Status
-                .Where(s => s.StatusType.StatusTypeName == Key.STATUS_TYPE_CLIENT
-                && s.StatusName == Key.STATUS_CLIENT_DISABLED)
-                .FirstOrDefault();
-            var bcitClient = new Client()
-            {
-                ClientName = "BCIT",
-                ReferenceID = Guid.NewGuid().ToString()
-            };
-            bcitClient.Status = bcitStatus;
-            context.Client.Add(bcitClient);
-            context.SaveChanges();
-
-            var ubcStatus = context.Status
-                .Where(s => s.StatusType.StatusTypeName == Key.STATUS_TYPE_CLIENT
-                && s.StatusName == Key.STATUS_CLIENT_ENABLED)
-                .FirstOrDefault();
-            var ubcClient = new Client()
-            {
-                ClientName = "UBC",
-                ReferenceID = Guid.NewGuid().ToString()
-            };
-
-            ubcClient.Status = ubcStatus;
-            context.Client.Add(ubcClient);
-            context.SaveChanges();
-        }
-
-        private void SeedUsers(ApplicationDbContext context)
-        {
-            // Get first client id
-            var clientID = context.Client.FirstOrDefault().ClientID;
-
             // Get user Statuses
             var userEnabledStatus = context.Status
                 .Where(s => s.StatusType.StatusTypeName == Key.STATUS_TYPE_USER
                 && s.StatusName == Key.STATUS_USER_ENABLED)
                 .FirstOrDefault();
-            var userDisabledStatus = context.Status
-                .Where(s => s.StatusType.StatusTypeName == Key.STATUS_TYPE_USER
-                && s.StatusName == Key.STATUS_USER_DISABLED)
-                .FirstOrDefault();
 
-            // Get send method
+            // Get default send method
             var sendMethodEmail = context.SendMethod.Where(m => m.SendMethodName == Key.SEND_METHOD_EMAIL).FirstOrDefault();
 
-            // Create a user on start
+            // Create a user
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
             var admin = new ApplicationUser()
             {
-                UserName = sampleAdminEmail,
-                Email = sampleAdminEmail,
+                UserName = adminEmail,
+                Email = adminEmail,
                 EmailConfirmed = true,
             };
             admin.UserDetail = new UserDetail()
             {
                 UserID = admin.Id,
                 StatusID = userEnabledStatus.StatusID,
-                FirstName = "John",
-                LastName = "White",
-                BusinessPhone = "604-778-9909",
-                MobilePhone = "778-990-2234",
-                HomePhone = "604-773-9908",
-                BusinessTitle = "Network Administrator",
+                FirstName = adminFirstName,
+                LastName = adminLastName,
+                BusinessPhone = null,
+                MobilePhone = null,
+                HomePhone = null,
+                BusinessTitle = null,
                 ReferenceID = Guid.NewGuid().ToString(),
                 SendMethodID = sendMethodEmail.SendMethodID
 
             };
-
-            var staff = new ApplicationUser()
-            {
-                UserName = "staff@portal.com",
-                Email = "staff@portal.com",
-                EmailConfirmed = true,
-            };
-            staff.UserDetail = new UserDetail()
-            {
-                UserID = staff.Id,
-                StatusID = userDisabledStatus.StatusID,
-                FirstName = "Amy",
-                LastName = "Lang",
-                BusinessPhone = "604-756-1239",
-                MobilePhone = "778-230-2349",
-                HomePhone = "604-433-9945",
-                BusinessTitle = "Client Manager",
-                ReferenceID = Guid.NewGuid().ToString(),
-                SendMethodID = sendMethodEmail.SendMethodID
-            };
-
-            var client = new ApplicationUser()
-            {
-                UserName = sampleClientEmail,
-                Email = sampleClientEmail,
-                EmailConfirmed = true,
-            };
-            client.UserDetail = new UserDetail()
-            {
-                UserID = client.Id,
-                StatusID = userDisabledStatus.StatusID,
-                ClientID = clientID,
-                FirstName = "Roland",
-                LastName = "Tang",
-                BusinessPhone = "604-223-2445",
-                MobilePhone = "778-223-4456",
-                HomePhone = "604-253-5567",
-                BusinessTitle = "Technical Director",
-                ReferenceID = Guid.NewGuid().ToString(),
-                SendMethodID = sendMethodEmail.SendMethodID
-            };
-
-            var user = new ApplicationUser()
-            {
-                UserName = sameplUserEmail,
-                Email = sameplUserEmail,
-                EmailConfirmed = true,
-            };
-            user.UserDetail = new UserDetail()
-            {
-                UserID = user.Id,
-                StatusID = userDisabledStatus.StatusID,
-                FirstName = "Patrick",
-                LastName = "McDonald",
-                BusinessPhone = "604-447-2356",
-                MobilePhone = "604-456-0090",
-                HomePhone = "778-123-2445",
-                BusinessTitle = "Project Manager",
-                ReferenceID = Guid.NewGuid().ToString(),
-                SendMethodID = sendMethodEmail.SendMethodID
-            };
-
-            userManager.Create(admin, "password");
-            userManager.Create(staff, "password");
-            userManager.Create(client, "password");
-            userManager.Create(user, "password");
+            
+            userManager.Create(admin, sampleAdminPassword);
 
             userManager.AddToRoles(admin.Id, Key.ROLE_ADMIN);
-            userManager.AddToRoles(staff.Id, Key.ROLE_STAFF);
-            userManager.AddToRoles(client.Id, Key.ROLE_CLIENT);
-            userManager.AddToRoles(user.Id, Key.ROLE_USER);
-        }
-
-        private void SeedServer(ApplicationDbContext context)
-        {
-            // get locationId
-            var location = context.DataCenterLocation
-                .Where(l => l.Location == Key.DATA_CENTER_LOCATION_VANCOUVER)
-                .FirstOrDefault();
-
-            // Get server Status
-            var serverOfflineStatus = context.Status
-                .Where(s => s.StatusType.StatusTypeName == Key.STATUS_TYPE_SERVER
-                && s.StatusName == Key.STATUS_SERVER_OFFLINE).FirstOrDefault();
-
-            // Get server types
-            var serverTypeApplication = context.ServerType
-                .Where(t => t.ServerTypeName == Key.SERVER_TYPE_APPLICATION)
-                .FirstOrDefault();
-            var serverTypeDirectory = context.ServerType
-                .Where(t => t.ServerTypeName == Key.SERVER_TYPE_DIRECTORY)
-                .FirstOrDefault();
-            var server = new Server()
-            {
-                ServerName = sampleServerName1,
-                Description = "Server for Domains",
-                LocationID = location.LocationID,
-                StatusID = serverOfflineStatus.StatusID,
-                ServerTypeID = serverTypeDirectory.ServerTypeID,
-                ReferenceID = Guid.NewGuid().ToString()
-            };
-            context.Server.Add(server);
-
-            server = new Server()
-            {
-                ServerName = sampleServerName2,
-                Description = "Server for Applications",
-                LocationID = location.LocationID,
-                StatusID = serverOfflineStatus.StatusID,
-                ServerTypeID = serverTypeApplication.ServerTypeID,
-                ReferenceID = Guid.NewGuid().ToString()
-            };
-            context.Server.Add(server);
-
-            server = new Server()
-            {
-                ServerName = sampleServerName3,
-                Description = "Server for more Applications",
-                LocationID = location.LocationID,
-                StatusID = serverOfflineStatus.StatusID,
-                ServerTypeID = serverTypeApplication.ServerTypeID,
-                ReferenceID = Guid.NewGuid().ToString()
-            };
-            context.Server.Add(server);
-
-            context.SaveChanges();
-        }
-
-        private void SeedApplication(ApplicationDbContext context)
-        {
-            // Get client userDetail
-            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-            var clientUser = userManager.FindByName(sampleClientEmail);
-            var clientUserDetail = context.UserDetail.Where(u => u.UserID == clientUser.Id);
-
-            // Get first client id
-            var clientID = context.Client.FirstOrDefault().ClientID;
-
-            // Get user userDetail
-            var userUser = userManager.FindByName(sameplUserEmail);
-            var userUserDetail = context.UserDetail.Where(u => u.UserID == userUser.Id);
-
-            // Get app Status
-            var appOfflineStatus = context.Status
-                .Where(s => s.StatusType.StatusTypeName == Key.STATUS_TYPE_APPLICATION
-                && s.StatusName == Key.STATUS_APPLICATION_OFFLINE).FirstOrDefault();
-            var appOnlineStatus = context.Status
-                .Where(s => s.StatusType.StatusTypeName == Key.STATUS_TYPE_APPLICATION
-                && s.StatusName == Key.STATUS_APPLICATION_ONLINE).FirstOrDefault();
-
-            // Get server
-            var serverApplication = context.Server
-                .Where(s => s.ServerType.ServerTypeName == Key.SERVER_TYPE_APPLICATION);
-            var serverDirectory = context.Server
-                .Where(s => s.ServerType.ServerTypeName == Key.SERVER_TYPE_DIRECTORY);
-
-            var app = new Application()
-            {
-                ApplicationName = sampleApplicationName1,
-                Description = "Portal to manage all your banking needs",
-                URL = "http://bank-app.com/",
-                ClientID = clientID,
-                StatusID = appOfflineStatus.StatusID,
-                ReferenceID = Guid.NewGuid().ToString()
-            };
-            app.UserDetails = clientUserDetail.ToList();
-            app.Servers = serverApplication.ToList();
-            context.Application.Add(app);
-
-            app = new Application()
-            {
-                ApplicationName = sampleApplicationName2,
-                Description = "App to handle all kinds of traffic",
-                URL = "http://traffic-app.com/",
-                ClientID = clientID,
-                StatusID = appOfflineStatus.StatusID,
-                ReferenceID = Guid.NewGuid().ToString()
-            };
-            app.UserDetails = clientUserDetail.ToList();
-            app.UserDetails = userUserDetail.ToList();
-            app.Servers = serverApplication.ToList();
-            context.Application.Add(app);
-
-            app = new Application()
-            {
-                ApplicationName = sampleApplicationName3,
-                Description = "Get your new domains",
-                URL = "http://domain-app.com/",
-                ClientID = clientID,
-                StatusID = appOnlineStatus.StatusID,
-                ReferenceID = Guid.NewGuid().ToString()
-            };
-            app.UserDetails = clientUserDetail.ToList();
-            app.Servers = serverDirectory.ToList();
-            context.Application.Add(app);
-
-            context.SaveChanges();
-        }
-
-        private void SeedNotification(ApplicationDbContext context)
-        {
-            // Get app
-            var apps = context.Application
-                .Where(a => a.ApplicationName == sampleApplicationName1);
-            // Get notification type
-            var notificationTypeMaintenance = context.NotificationType
-                .Where(t => t.NotificationTypeName == Key.NOTIFICATION_TYPE_MAINTENANCE)
-                .FirstOrDefault();
-            var notificationTypeIncident = context.NotificationType
-                .Where(t => t.NotificationTypeName == Key.NOTIFICATION_TYPE_INCIDENT)
-                .FirstOrDefault();
-            // Get levelOfImpact
-            var levelOfImpactNonImpacting = context.LevelOfImpact
-                .Where(l => l.LevelName == Key.LEVEL_OF_IMPACT_NON_IMPACTING)
-                .FirstOrDefault();
-            var levelOfImpactImpacting = context.LevelOfImpact
-                .Where(l => l.LevelName == Key.LEVEL_OF_IMPACT_IMPACTING)
-                .FirstOrDefault();
-            // Get server
-            var servers = context.Server.Where(s => s.ServerName == sampleServerName1);
-            // Get status
-            var statusOpen = context.Status
-                .Where(s => s.StatusType.StatusTypeName == Key.STATUS_TYPE_NOTIFICATION
-                && s.StatusName == Key.STATUS_NOTIFICATION_OPEN)
-                .FirstOrDefault();
-            var statusClosed = context.Status
-                .Where(s => s.StatusType.StatusTypeName == Key.STATUS_TYPE_NOTIFICATION
-                && s.StatusName == Key.STATUS_NOTIFICATION_CLOSED)
-                .FirstOrDefault();
-            // Get priority
-            var priorityHigh = context.Priority
-                .Where(p => p.PriorityName == Key.PRIORITY_NAME_HIGH)
-                .FirstOrDefault();
-            var priorityNormal = context.Priority
-                .Where(p => p.PriorityName == Key.PRIORITY_NAME_NORMAL)
-                .FirstOrDefault();
-            var priorityLow = context.Priority
-                .Where(p => p.PriorityName == Key.PRIORITY_NAME_LOW)
-                .FirstOrDefault();
-            // Get Creator
-            var admin = context.Users.Where(u => u.Email == sampleAdminEmail).FirstOrDefault();
-
-            string sampleThread1 = "MAI-4561483";
-            var notification = new Notification()
-            {
-                StartDateTime = DateTime.Now.AddHours(-1),
-                EndDateTime = DateTime.Now,
-                SentDateTime = DateTime.Now.AddHours(-2),
-                NotificationHeading = "Server offline",
-                NotificationDescription = "Server will be offline for maintenance",
-                NotificationTypeID = notificationTypeMaintenance.NotificationTypeID,
-                LevelOfImpactID = levelOfImpactNonImpacting.LevelOfImpactID,
-                StatusID = statusOpen.StatusID,
-                IncidentNumber = sampleThread1,
-                PriorityID = priorityHigh.PriorityID,
-                ReferenceID = Guid.NewGuid().ToString(),
-                UserID = admin.Id
-            };
-            notification.Servers = servers.ToList();
-            context.Notification.Add(notification);
-
-            notification = new Notification()
-            {
-                StartDateTime = DateTime.Now.AddHours(-1),
-                EndDateTime = DateTime.Now,
-                SentDateTime = DateTime.Now,
-                NotificationHeading = "Server back online",
-                NotificationDescription = "Maintenance complete",
-                NotificationTypeID = notificationTypeMaintenance.NotificationTypeID,
-                LevelOfImpactID = levelOfImpactNonImpacting.LevelOfImpactID,
-                StatusID = statusClosed.StatusID,
-                IncidentNumber = sampleThread1,
-                PriorityID = priorityLow.PriorityID,
-                ReferenceID = Guid.NewGuid().ToString(),
-                UserID = admin.Id
-            };
-            notification.Servers = servers.ToList();
-            context.Notification.Add(notification);
-
-            notification = new Notification()
-            {
-                SentDateTime = DateTime.Now,
-                NotificationHeading = "Crashed Application",
-                NotificationDescription = "Application has crashed",
-                NotificationTypeID = notificationTypeIncident.NotificationTypeID,
-                LevelOfImpactID = levelOfImpactImpacting.LevelOfImpactID,
-                StatusID = statusOpen.StatusID,
-                IncidentNumber = "INC-6928376",
-                PriorityID = priorityNormal.PriorityID,
-                ReferenceID = Guid.NewGuid().ToString(),
-                UserID = admin.Id
-            };
-            notification.Applications = apps.ToList();
-            notification.Servers = servers.ToList();
-            context.Notification.Add(notification);
-
-            context.SaveChanges();
         }
 
         private void RemoveAll(ApplicationDbContext context)
